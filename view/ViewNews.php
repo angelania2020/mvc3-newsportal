ef80718067edc82bd4722741b3bd966435cb41ec
Angelina <angelania@ya.ru>
2021-12-08 19:23:58 +0200

    added first version of newsportal with menu and news by categories

---
+++ view/ViewNews.php
@@ -0,0 +1,46 @@
+<?php
+class ViewNews
+{
+
+    public static function NewsByCategory($arr)
+    {
+        foreach ($arr as $value) {
+            echo '<img src="data:image/jpeg;base64,' . base64_encode($value['picture']) . '" width=150 /><br>';
+            echo "<h2>" . $value['title'] . "</h2>";
+            echo "<a href='news?id=" . $value['id'] . "'>Edasi</a><br>";
+        }
+    }
+
+    public static function AllNews($arr)
+    {
+        foreach ($arr as $value) {
+            echo "<li>" . $value['title'];
+            echo "<a href='news?id=" . $value['id'] . "'>Edasi</a></li><br>";
+        }
+    }
+
+    public static function ReadNews($n)
+    {
+        echo "<h2>" . $n['title'] . "</h2>";
+        echo '<br><img src="data:image/jpeg;base64,' . base64_encode($n['picture']) . '" width=150/><br>';
+        echo "<p>" . $n['text'] . "</p>";
+    }
+
+    public static function PictureTitleNews($arr)
+    {
+        foreach ($arr as $value) {
+            echo "<div class='row'>";
+            echo "<a href='news?id=" . $value['id'] . "'><img src='data:image/jpeg;base64," . base64_encode($value['picture']) . "' width=150 class='img-thumbnail'></a>";
+            echo "<a href='news?id=" . $value['id'] . "' style='display: flex; justify-content: center; flex-direction: column;'>" . $value['title'] . "</a><br>";
+            echo "</div><br>";
+        }
+    }
+
+    public static function ColorfulNews($arr)
+    {
+        foreach ($arr as $value) {
+            echo "<h1 style='background-color:#ab00007a;'>" . $value['title'] . "</h1>";
+            echo "<p style='background-color:#ffdfdf;'>" . $value['text'] . "</p><br>";
+        }
+    }
+}
---
+++ view/start.php
@@ -0,0 +1,14 @@
+<?php
+ob_start();
+?>
+<h1>TOP 3 UUDIST</h1>
+<br>
+<?php
+
+ViewNews::NewsByCategory($arr);
+
+$content = ob_get_clean();
+
+include_once 'view/layout.php';
+
+?>
